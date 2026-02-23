ef27e83182132ebc3dfde49be5e62a6b1597de24
OyudiF <ochiaifelipe@gmail.com>
2026-02-23 13:46:08 -0300

    Primeiro commit do projeto

---
+++ src/ContadorFila.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class ContadorFila {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("Contador de Senhas");
+
+        janela.setSize(300, 200);
+
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        janela.setVisible(true);
+    }
+}
