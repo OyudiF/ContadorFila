c1770026568075b833be4cc545dbdb1f852bf12a
OyudiF <ochiaifelipe@gmail.com>
2026-02-23 14:17:37 -0300

    Primeira versão

--- src/ContadorFila.java
+++ src/ContadorFila.java
@@ -1,13 +1,41 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class ContadorFila {
     public static void main(String[] args) {
+        // Vars
         JFrame janela = new JFrame("Contador de Senhas");
+        JLabel labelNum = new JLabel("0");
+        JPanel panel = new JPanel();
+        JButton buttonNext = new JButton("Avançar");
+        JButton buttonBack = new JButton("Voltar");
+        JButton buttonReset = new JButton("Zerar");
+        Font fonteMaior = new Font("Arial", Font.BOLD, 80);
+        Font fonteMedia = new Font("Arial", Font.BOLD, 35);
 
-        janela.setSize(300, 200);
+        // Configurando labels
+        labelNum.setFont(fonteMaior);
+        labelNum.setHorizontalAlignment(SwingConstants.CENTER);
 
+        // Janela Configs
+        janela.setSize(800, 480);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLayout(new BorderLayout());
 
+        // Botões
+        buttonBack.setFont(fonteMedia);
+        buttonReset.setFont(fonteMedia);
+        buttonNext.setFont(fonteMedia);
+
+        panel.add(buttonBack);
+        panel.add(buttonReset);
+        panel.add(buttonNext);
+
+        // Adicionando na Janela
+        janela.add(labelNum, BorderLayout.CENTER);
+        janela.add(panel, BorderLayout.SOUTH);
+
+        // Desenha a tela
         janela.setVisible(true);
     }
 }
